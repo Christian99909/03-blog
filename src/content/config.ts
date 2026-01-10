a2e52bdfc37f90d4f3a75dea03254a5d46d523f2
Christian99909 <cristian99909@gmail.com>
2026-01-09 23:42:49 -0600

    add authorCollection

--- src/content/config.ts
+++ src/content/config.ts
@@ -19,7 +19,17 @@ const blogCollection = defineCollection({
 });
 
 
+const authorCollection = defineCollection({
+    type: 'data',
+    schema: ({ image }) => z.object({
+        name: z.string(),
+        avatar: image(),
+    })
+})
+
+
 export const collections = {
 
     blog: blogCollection,
+    author: authorCollection,
 };
